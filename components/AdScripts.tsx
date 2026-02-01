569d95ac5e93a3711d39b24a1148d845a988fac3
tracksatscale <khilarir68@gmail.com>
2026-02-01 21:56:00 +0530

    feat: enable Monetag inpage push ads script

--- components/AdScripts.tsx
+++ components/AdScripts.tsx
@@ -47,12 +47,10 @@ export function AdScripts() {
             </Script>
             */}
 
-            {/* Monetag Inpage Push Ads - REMOVED */}
-            {/*
+            {/* Monetag Inpage Push Ads */}
             <Script id="monetag-inpage-push" strategy="afterInteractive">
                 {`(function(s){s.dataset.zone='10450838',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`}
             </Script>
-            */}
         </>
     )
 }
