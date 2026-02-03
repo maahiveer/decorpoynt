b1545595b1df9d28942b79dffa77025fc3a55aa1
tracksatscale <khilarir68@gmail.com>
2026-02-03 12:50:22 +0530

    Add Google Analytics tag

--- components/AdScripts.tsx
+++ components/AdScripts.tsx
@@ -59,6 +59,21 @@ export function AdScripts() {
                 data-cfasync="false"
                 strategy="afterInteractive"
             />
+
+            {/* Google tag (gtag.js) */}
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-E0JTRM3ZZ7"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-E0JTRM3ZZ7');
+        `}
+            </Script>
         </>
     )
 }
